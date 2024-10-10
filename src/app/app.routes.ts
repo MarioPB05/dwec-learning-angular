e77f433411d7014f0672b595aa319b204c4ec30a
Mario <marioperdiguero37@gmail.com>
2024-10-10 16:46:16 +0200

    🎉 Initialize Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SaludoComponent } from '../components/saludo/saludo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'exercise-1',
+    component: SaludoComponent
+  }
+];

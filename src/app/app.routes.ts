4192c242937677879347fb3af9a878a02a0f33b6
Mario <marioperdiguero37@gmail.com>
2024-10-25 14:52:57 +0200

    ✨ Add exercises routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { TaskListComponent } from '../components/task-list/task-list.component';
 import { TemporizadorComponent } from '../components/temporizador/temporizador.component';
 import { ComentariosComponent } from '../components/comentarios/comentarios.component';
 import { ProductosComponent } from '../components/productos/productos.component';
+import { CalculadoraComponent } from '../components/calculadora/calculadora.component';
+import {ListaCompraComponent} from '../components/lista-compra/lista-compra.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,5 +16,7 @@ export const routes: Routes = [
   { path: 'exercise-3', component: TaskListComponent },
   { path: 'exercise-4', component: TemporizadorComponent },
   { path: 'exercise-5', component: ComentariosComponent },
-  { path: 'exercise-6', component: ProductosComponent }
+  { path: 'exercise-6', component: ProductosComponent },
+  { path: 'exercise-7', component: CalculadoraComponent },
+  { path: 'exercise-8', component: ListaCompraComponent }
 ];
